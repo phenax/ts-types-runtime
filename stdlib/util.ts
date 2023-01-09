592f866637bfb8ef8a4a5c31e0984abf6df82532
Akshay Nair <phenax5@gmail.com>
2023-01-09 19:08:10 +0530

    feat: adds some utility types

---
+++ stdlib/util.ts
@@ -0,0 +1,22 @@
+import { Kind1 } from './io'
+
+export type Let<f extends (...args: any) => any> = ReturnType<f>
+
+export type Apply<K extends Kind1, Val> = (K & { input: Val })['return']
+
+export interface Id extends Kind1 {
+  return: this['input']
+}
+
+type ADTTT = { _type: string; value: any }
+
+interface ADTConstructor<A extends ADTTT> extends Kind1<A['value']> {
+  return: this['input'] extends infer Inp ? A & { value: Inp } : A
+}
+
+type Pat = Record<string, ADTTT>
+export type ADT<D extends Record<string, any>> = {
+  [k in keyof D]: { _type: k; value: D[k] }
+} extends infer Rec extends Pat
+  ? Rec[keyof Rec] & { [k in keyof Rec]: ADTConstructor<Rec[k]> }
+  : never
